ad7b31fca5737b06c7347f9fcf264da64475f9e5
Vo Xuan Mai Hoang <hoang.vo@hearti.io>
2021-04-02 14:52:14 +0700

    Update admin page

--- src/admin/ResponsiveDrawer.jsx
+++ src/admin/ResponsiveDrawer.jsx
@@ -1,21 +1,19 @@
-import React from "react";
-import PropTypes from "prop-types";
 import AppBar from "@material-ui/core/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Divider from "@material-ui/core/Divider";
 import Drawer from "@material-ui/core/Drawer";
 import Hidden from "@material-ui/core/Hidden";
-import IconButton from "@material-ui/core/IconButton";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import MailIcon from "@material-ui/icons/Mail";
-import MenuIcon from "@material-ui/icons/Menu";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import PropTypes from "prop-types";
+import React from "react";
+import logo from "../../src/assets/images/commons/logo_lagre.png";
 
 const drawerWidth = 240;
 
@@ -41,14 +39,18 @@ const useStyles = makeStyles((theme) => ({
       display: "none",
     },
   },
-  // necessary for content to be below app bar
   toolbar: theme.mixins.toolbar,
   drawerPaper: {
     width: drawerWidth,
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3)
+    padding: theme.spacing(3),
+    height: 750,
+    backgroundImage:
+      'url("https://images.unsplash.com/photo-1514845994104-1be22149278b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80")',
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
   },
 }));
 
@@ -56,25 +58,29 @@ function ResponsiveDrawer(props) {
   const { window } = props;
   const classes = useStyles();
   const theme = useTheme();
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
 
   const drawer = (
     <div>
-      <div className={classes.toolbar} />
+      <div className={classes.toolbar}>
+        <img
+          className="logo"
+          src={logo}
+          alt="logo"
+          style={{ marginLeft: "15%" }}
+        />
+      </div>
       <Divider />
       <List>
-        {["Bevarages", "Employees", "Users"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        {["Bevarages", "Employees", "Users", "Calendar", "Salary"].map(
+          (text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          )
+        )}
       </List>
     </div>
   );
@@ -85,41 +91,32 @@ function ResponsiveDrawer(props) {
   return (
     <div className={classes.root}>
       <CssBaseline />
+
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            className={classes.menuButton}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" noWrap>
             Coffee Managements
           </Typography>
         </Toolbar>
       </AppBar>
+
       <nav className={classes.drawer} aria-label="mailbox folders">
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Hidden smUp implementation="css">
           <Drawer
             container={container}
             variant="temporary"
             anchor={theme.direction === "rtl" ? "right" : "left"}
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
             classes={{
               paper: classes.drawerPaper,
             }}
             ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
+              keepMounted: true,
             }}
           >
             {drawer}
           </Drawer>
         </Hidden>
+        
         <Hidden xsDown implementation="css">
           <Drawer
             classes={{
@@ -134,42 +131,12 @@ function ResponsiveDrawer(props) {
       </nav>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-          Coffee is one of the world’s most popular beverages. Some claim it is
-          the most widely consumed liquid in the world aside from water. Coffee
-          is more than a beverage, however. It is a memory, an anticipation, a
-          lifetime of consoling moments of modest pleasure woven into our lives.
-          Coffee’s success as a beverage undoubtedly is owing both to the
-          caffeine it harbors and to its sensory pleasure. Coffee lovers come to
-          associate the energizing lift of the caffeine with the richness and
-          aroma of the beverage that delivers it. Coffee is produced from the
-          seeds of a small red (sometimes yellow) fruit that grows on plants
-          halfway in size between shrub and tree. The process that turns these
-          seeds into beverage is a long and complex process, perhaps the most
-          complex process associated with any major beverage. It is also a very
-          labor intensive process involving a vast intercontinental
-          collaboration that starts with the coffee grower, moves from there to
-          the picker, then to the mill workers who meticulously remove the fruit
-          and dry the beans, then to those who clean and grade the beans, to
-          those who roast them, to those consumers and baristas who finally
-          grind the beans and prepare the beverage. Every act along the way can
-          be performed either with passion and precision or with careless
-          shoddiness. It is the cumulative quality of all of these creative
-          contributions that together make the difference between a lackluster
-          cup and a fine and distinctive one. Coffee Review is devoted to
-          promoting the fine, distinctive cup and celebrating its lore and
-          pleasures.
-        </Typography>
       </main>
     </div>
   );
 }
 
 ResponsiveDrawer.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
   window: PropTypes.func,
 };
 
