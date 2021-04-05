0addaefa398a5e2242f92c2d1800a4ad9c567fcc
Vo Xuan Mai Hoang <hoang.vo@hearti.io>
2021-04-05 15:59:29 +0700

    merge change

--- src/admin/ResponsiveDrawer.jsx
+++ src/admin/ResponsiveDrawer.jsx
@@ -6,7 +6,6 @@ import Hidden from "@material-ui/core/Hidden";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
